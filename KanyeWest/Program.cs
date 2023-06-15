f084fb031435307ed72a1c0c2bf09483543dadf8
Seth Bowman <brandonsethbowman@gmail.com>
2023-06-15 11:46:55 -0500

    Add project files.

---
+++ KanyeWest/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+using Newtonsoft.Json.Linq;
+
+var client = new HttpClient();
+
+var kanyeURL = "https://api.kanye.rest/";
+
+var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+//Console.WriteLine(kanyeResponse);
+
+var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+Console.WriteLine(kanyeQuote);
+
+
